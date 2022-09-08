d56a65127d2c527d93226c01942676b7c869c3ea
dirtyavenger <maresek96@gmail.com>
2022-09-08 15:06:36 +0200

    My imlementation of expected funtionality

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.close-modal');
+
+console.log(buttons);
+console.log(buttons.length);
+
+buttons.forEach(function (item, index) {
+  item.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+});
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+overlay.addEventListener('click', closeModal);
+closeButton.addEventListener('click', closeModal);
