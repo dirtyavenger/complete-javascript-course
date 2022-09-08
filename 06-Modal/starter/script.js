2a6e5c835ed4e622bfbf93aa44375fb29502962e
dirtyavenger <maresek96@gmail.com>
2022-09-08 19:01:01 +0200

    My Final modal implementation

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,19 +5,25 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeButton = document.querySelector('.close-modal');
 
-console.log(buttons);
-console.log(buttons.length);
-
-buttons.forEach(function (item, index) {
-  item.addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-});
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+buttons.forEach(function (item, index) {
+  item.addEventListener('click', function () {
+    openModal();
+  });
+});
+
 overlay.addEventListener('click', closeModal);
 closeButton.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
