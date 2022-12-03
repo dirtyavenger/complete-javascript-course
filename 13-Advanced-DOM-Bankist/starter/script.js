be5b94c839d0ded11590b5b9b2ed7c8345aa368c
Marek Kaul <marek.kaul@external.erstegroup.com>
2022-12-03 18:02:41 +0100

    Upratovanie

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -48,6 +48,78 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+const header = document.querySelector('.header');
+header.prepend(message);
+const button = message.lastElementChild;
+console.log(button);
+button.addEventListener('click', function () {
+  message.remove();
+  console.log('kokot');
+});
+
+/* const buttons = document.getElementsByTagName('button');
+console.log(buttons);
+buttons.forEach(button => {
+  console.log('button');
+  addEventListener('click', () => console.log('Button pressed'));
+}); */
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const chars = ['a', 'b', 'c'];
+let array = reverseLetters(chars);
+function reverseLetters(chars) {
+  return chars.sort();
+}
+console.log(array);
+
 const kokot = oc => {
   return oc;
 };
+
+tabButtonsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+  this.querySelectorAll('.operations__tab').forEach(el =>
+    el.classList.remove('operations__tab--active')
+  );
+
+  clicked.classList.add('operations__tab--active');
+
+  const tabs = clicked.closest('.operations').children;
+
+  [...tabs].forEach(el => el.classList.remove('operations__content--active'));
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+const mouseOver = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    e.target
+      .closest('.nav')
+      .querySelectorAll('.nav__link')
+      .forEach(el => {
+        console.log(el.textContent);
+        if (el != e.target) {
+          el.style.opacity = this;
+        }
+      });
+    e.target.closest('.nav').querySelector('img').style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', mouseOver.bind(0.5));
+
+nav.addEventListener('mouseout', mouseOver.bind(1));
+
+window.addEventListener('scroll', function (e) {
+  console.log(document.querySelector('.header').offsetHeight);
+  window.scrollY > document.querySelector('.header').offsetHeight
+    ? nav.closest('.nav').classList.add('sticky')
+    : nav.closest('.nav').classList.remove('sticky');
+});
